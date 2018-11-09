d58cba1fe545ef6e803d421df6be20a2579299df
= <=>
2018-11-09 09:41:40 -0200

    novos models inseridos e model usuario modificado

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,29 @@
 class Usuario < ActiveRecord::Base
   has_many :protocolos
 
+  has_many :tipo_vinculos
+  has_many :logs
+
+  has_many :permitidos, dependent: :destroy
+  has_many :perfils, through: :permitidos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
   has_secure_password
 
   def self.authenticate(email, password)
@@ -9,5 +32,7 @@ class Usuario < ActiveRecord::Base
         try(:authenticate, password)
 
   end
+
+=end
 
 end
